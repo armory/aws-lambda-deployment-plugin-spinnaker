abaf9269508f0ee490937255f83c6028d41953b3
Avi1235 <david-prz98@hotmail.com>
2022-07-08 16:20:11 -0600

    stash -- not working

---
+++ lambda-deployment-orca/src/test/kotlin/com/amazon/aws/spinnaker/plugin/lambda/traffic/LambdaTrafficRoutingStageFixture.kt
@@ -0,0 +1,41 @@
+package com.amazon.aws.spinnaker.plugin.lambda.traffic
+
+import com.amazon.aws.spinnaker.plugin.lambda.utils.LambdaCloudDriverUtils
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.netflix.spinnaker.kork.plugins.internal.PluginJar
+import com.netflix.spinnaker.orca.StageResolver
+import com.netflix.spinnaker.orca.api.test.OrcaFixture
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.test.context.TestPropertySource
+import org.springframework.test.web.servlet.MockMvc
+import java.io.File
+
+@TestPropertySource(properties = [
+    "spinnaker.extensibility.plugins.Aws.LambdaDeployment.Plugin.enabled=true",
+    "spinnaker.extensibility.plugins-root-path=build/plugins"
+])
+@AutoConfigureMockMvc
+ class OrcaPluginsFixture : OrcaFixture() {
+    @Autowired
+    lateinit var stageResolver: StageResolver
+
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    val mapper = jacksonObjectMapper().disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+
+    init {
+        val pluginId = "Aws.LambdaDeploymentPlugin"
+        val plugins = File("build/plugins").also {
+            it.delete()
+            it.mkdir()
+        }
+
+        PluginJar.Builder(plugins.toPath().resolve("$pluginId.jar"), pluginId)
+            .pluginClass(LambdaTrafficRoutingStage::class.java.name)
+            .pluginVersion("1.0.0")
+            .build()
+    }
+}
---
+++ lambda-deployment-orca/src/test/kotlin/com/amazon/aws/spinnaker/plugin/lambda/traffic/LambdaTrafficRoutingStageIntegrationTest.kt
@@ -0,0 +1,84 @@
+package com.amazon.aws.spinnaker.plugin.lambda.traffic
+
+import com.amazon.aws.spinnaker.plugin.lambda.traffic.model.LambdaBlueGreenStrategyInput
+import com.amazon.aws.spinnaker.plugin.lambda.traffic.model.LambdaTrafficUpdateInput
+import com.amazon.aws.spinnaker.plugin.lambda.upsert.model.LambdaConcurrencyInput
+import com.amazon.aws.spinnaker.plugin.lambda.utils.LambdaDefinition
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.netflix.spinnaker.orca.api.test.orcaFixture
+import dev.minutest.junit.JUnit5Minutests
+import org.springframework.http.MediaType
+import dev.minutest.rootContext
+import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.post
+import strikt.api.expect
+import strikt.assertions.isEqualTo
+
+class LambdaTrafficRoutingStageIntegrationTest: JUnit5Minutests {
+
+    fun tests() = rootContext<OrcaPluginsFixture> {
+        context("a running Orca instance") {
+            orcaFixture {
+                OrcaPluginsFixture()
+            }
+
+            test("LambdaTrafficRoutingStage extension is resolved to the correct type") {
+                val stageDefinitionBuilder = stageResolver.getStageDefinitionBuilder(
+                    LambdaTrafficRoutingStage::class.java.name, "Aws.LambdaTrafficRoutingStage")
+
+                expect {
+                    that(stageDefinitionBuilder.type).isEqualTo("Aws.LambdaTrafficRoutingStage")
+                }
+            }
+
+            test("LambdaTrafficRoutingStage can be executed as a stage within a live pipeline execution") {
+                val response = mockMvc.post("/orchestrate") {
+                    contentType = MediaType.APPLICATION_JSON
+                    content = mapper.writeValueAsString(mapOf(
+                        "application" to "lambda",
+                        "stages" to listOf(mapOf(
+                            "refId" to "1",
+                            "type" to "Aws.LambdaTrafficRoutingStage",
+                            "functionName" to "lambda-myLambda",
+                            "region" to "us-west-2",
+                            "deploymentStrategy" to "\$BLUEGREEN",
+                            "payload" to "payload",
+                            "timeout" to 30,
+                            "provisionedConcurrentExecutions" to 12
+                        ))
+                    ))
+                }.andReturn().response
+
+                expect {
+                    that(response.status).isEqualTo(200)
+                }
+
+                val ref = mapper.readValue<ExecutionRef>(response.contentAsString).ref
+
+                var execution: Execution
+                do {
+                    execution = mapper.readValue(mockMvc.get(ref).andReturn().response.contentAsString)
+                } while (execution.status != "SUCCEEDED")
+
+                expect {
+                    that(execution)
+                        .get { stages.first() }
+                        .and {
+                            get { type }.isEqualTo("Aws.LambdaTrafficRoutingStage")
+                            get { status }.isEqualTo("SUCCEEDED")
+                            get { context.functionName }.isEqualTo("lambda-myLambda")
+                            get { context.region }.isEqualTo("us-west-2")
+                            get { context.deploymentStrategy }.isEqualTo("\$BLUEGREEN")
+                            get { context.payload }.isEqualTo("payload")
+                            get { context.timeout }.isEqualTo(30)
+                            get { concurrency.provisionedConcurrentExecutions }.isEqualTo(12)
+                        }
+                }
+            }
+        }
+    }
+
+    data class ExecutionRef(val ref: String)
+    data class Execution(val status: String, val stages: List<Stage>)
+    data class Stage(val status: String, val context: LambdaTrafficUpdateInput, val concurrency:LambdaConcurrencyInput, val type: String)
+}
